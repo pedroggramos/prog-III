2086ec67471c3046b78dee65f1e1c931024b5961
Pedro Ramos <pedrogabrielguimaraesramos178@gmail.com>
2025-02-28 15:36:21 -0300

    up

--- studies/aula3/CtrlPrograma.java
+++ studies/aula3/CtrlPrograma.java
@@ -3,20 +3,27 @@ package controller;
 import model.Departamento;
 
 public class CtrlPrograma {
-    
-    public static void main(String[] args){
-        Departamento d1 = new Departamento("CB", "Contabilidade");
-        System.out.println(d1);
 
-        System.out.println("A sigla de d1: é " + d1.getSigla());
-        System.out.println("O nome de d1: é " + d1.getNome());
-
-        d1.setSigla("CBE");
-        d1.setNome("Contabilidade Empresarial");
-
-        System.out.println("A sigla d1 é: " + d1.getSigla());
-        System.out.println("O nome de d1 é: " + d1.getNome());
-
-    }
+	public static void main(String[] args) {
+		// Declaração da variável 'd1' (ponteiro para Departamento)
+		// e Instanciação de um Objeto Departamento 
+		Departamento d1 = new Departamento("CB","Contabilidade");
+		System.out.println(d1);
+		
+		// Enviamos a mensagem 'getSigla()' para o objeto apontado por d1
+		System.out.println("A sigla de d1 é: "  +   d1.getSigla());
+		// Enviamos a mensagem 'getNome()' para o objeto apontado por d1
+		System.out.println("O nome de d1 é: "   +   d1.getNome());
+				
+		// Tentamos realizar as alterações, mas não iremos conseguir
+		// pois as Strings são inválidas
+		d1.setSigla("123");
+		d1.setNome("");
+		
+		// Vamos verificar que as alterações não foram promovidas
+		// pois os valores eram inválidos
+		System.out.println("A sigla de d1 é: "  +   d1.getSigla());
+		System.out.println("O nome de d1 é: "   +   d1.getNome());
+	}
 
 }
--- studies/aula3/Departamento.java
+++ studies/aula3/Departamento.java
@@ -1,51 +1,109 @@
 package model;
 
+public class Departamento {
+	//
+	// ATRIBUTOS
+	//
+	private String sigla;
+	private String nome;
+	
+	//
+	// MÉTODOS
+	//
+	/**
+	 * Método construtor de Departamento
+	 */
+	public Departamento(String sigla, String nome) {
+		super();
+		// Enviamos a mensagem 'setSigla' para o objeto 
+		// que estiver executando este construtor.
+		this.setSigla(sigla);
+		// Enviamos a mensagem 'setNome' para o objeto 
+		// que estiver executando este construtor.
+		this.setNome(nome);
+	}
 
-public class Departamento{
+	/**
+	 * Retorna a sigla do Deparamento
+	 */
+	public String getSigla() {
+		return this.sigla;
+	}
 
-    // Atributos 
+	/**
+	 * Altera a sigla do Departamento, desde que o parâmetro seja válido
+	 */
+	public void setSigla(String sigla) {
+		// Se o parâmetro 'sigla' é válido, fazemos a atribuição
+		if(this.validarSigla(sigla))
+			this.sigla = sigla;
+	}
 
-    private String sigla;
-    private String nome;
+	/**
+	 * Retorna o nome do Departamento
+	 */
+	public String getNome() {
+		return this.nome;
+	}
 
+	/**
+	 * Altera o nome do Departamento, desde que o parâmetro seja válido
+	 */
+	public void setNome(String nome) {
+		// Se o parâmetro 'nome' é válido, fazemos a atribuição
+		if(this.validarSigla(nome))
+			this.nome = nome;
+	} 
 
-    // Métodos
-
-    public Departamento(String s, String n){
-        super();
-        this.s = sigla;
-        this.n = nome;
-    }
-
-
-    public String getSigla() {
-        return sigla;
-    }
-
-
-    public void setSigla(String sigla) {
-        this.sigla = sigla;
-    }
-
-
-    public String getNome() {
-        return nome;
-    }
-
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-}
-
-public boolean validarSigla(String sigla){
-    if(sigla == null || sigla.length()) == 0)
-        return false;
-    for(int i = 0; i < sigla.length(); i++){
-        char c = sigla.charAt(i);
-        if( ! Character.isAlphabetic(c))
-            return false;
-    }
-    return true;
+	/**
+	 * Verifica se o parâmetro passado corresponde a uma sigla válida
+	 */
+	// TODO Essa forma de validação será refeita na próxima aula
+	public boolean validarSigla(String sigla) {
+		// Se 'sigla' aponta para null OU se o tamanho da String apontado por 'sigla'
+		// é zero, retornamos 'false'. Observe que 'sigla.length()' corresponde ao 
+		// envio da mensagem 'length()' para o objeto apontado por 'sigla'
+		if(sigla == null || sigla.length() == 0)
+			return false; 
+		// Vamos verificar cada caracter na String apontada por sigla
+		for(int i = 0; i < sigla.length(); i++) {
+			// A mensagem 'charAt(i)' retorna o caracter presente na posição 'i'
+			char c = sigla.charAt(i);
+			// Se o caracter NÃO é alfabético, retornamos 'false'
+			if( ! Character.isAlphabetic(c))
+				return false;
+		}
+		// Todas as verificações foram feitas. Retornamos 'true'
+		return true;
+	}
+	
+	/**
+	 * Verifica se o parâmetro passado corresponde a um nome válido
+	 */
+	// TODO Essa forma de validação será refeita na próxima aula
+	public boolean validarNome(String nome) {
+		// Se 'nome' aponta para null OU se o tamanho da String apontado por 'nome'
+		// é zero, retornamos 'false'. Observe que 'nome.length()' corresponde ao 
+		// envio da mensagem 'length()' para o objeto apontado por 'nome'
+		if(nome == null || nome.length() == 0)
+			return false; 
+		// Vamos verificar cada caracter na String apontada por 'nome'
+		for(int i = 0; i < nome.length(); i++) {
+			// A mensagem 'charAt(i)' retorna o caracter presente na posição 'i'
+			char c = nome.charAt(i);
+			// Se o caracter NÃO é alfabético E também não é espaço em branco
+			// retornamos 'false'
+			if( !Character.isAlphabetic(c) && !Character.isSpaceChar(c) )
+				return false;
+		}
+		// Todas as verificações foram feitas. Retornamos 'true'
+		return true;
+	}	
+	//
+	// Observação: Se você gerar o método construtor e os métodos gets/sets com o 
+	// Eclipse, não deixe de fazer as seguintes alterações:
+	// (1) Acrescente o "this." nos métodos get na linha onde tem o return.
+	// (2) No construtor, troque as atribuições pelo uso dos métodos 'set'
+	// (3) Nos métodos 'set' só faça a atribuição após testar o parâmetro
+	//     usando o método 'validar' adequado.		
 }
